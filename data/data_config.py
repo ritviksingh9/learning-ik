0b231f896b98aa15453a015118499cd904ecd72d
ritviksingh9 <ritviksingh9@gmail.com>
2021-07-10 11:19:24 -0400

    Added dataset generation functionality

---
+++ data/data_config.py
@@ -0,0 +1,15 @@
+import dataclasses
+
+@dataclasses.dataclass
+class DataGenConfig:
+    """
+    @brief Defines basic configuration parameters for generating the dataset.
+    """
+    # Basic robot model information 
+    ROBOT: str = "franka"
+    ROBOT_URDF: str = "panda_arm.urdf"
+    EE_NAME: str = "panda_link7"
+    # Config for the dataset
+    OUT_FILE_NAME: str = "franka_ik_data.txt"
+    IS_QUAT: bool = False
+    NUM_DATA: int = 10
